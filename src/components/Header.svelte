00743a4b64158918199b70ec30069ffea31f7b1b
garedan <danyG22@proton.me>
2023-06-28 22:59:56 -0300

    catsgram

---
+++ src/components/Header.svelte
@@ -0,0 +1,73 @@
+<script>
+  import { likeCount } from "../store/store.js"; 
+</script>
+
+<style>
+  .Header {
+    background-color: white;
+    position: fixed;
+    width: 100%;
+  }
+  .Header-container {
+    grid-template-columns: minmax(auto, 936px);
+    display: grid;
+    justify-content: center;
+    background-color: white;
+    border-bottom: 1px solid rgba(38, 38, 38, 0.4);
+  }
+  .Header-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5em;
+  }
+  .Header ul {
+    list-style: none;
+  }
+  .Header li {
+    display: inline-block;
+    margin: 0 0 0 1em;
+  }
+
+  h1 {
+    padding: 0;
+    margin: 0;
+    font-size: 28px;
+    color: black;
+    font-family: "Pacifico", cursive;
+    cursor: pointer;
+    position: relative;
+    background: #fff;
+    mix-blend-mode: multiply;
+    display: inline-block;
+  }
+
+  h1:before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(circle at top left, #f09433, #bc1888);
+    mix-blend-mode: screen;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
+
+<div class="Header">
+  <div class="Header-container">
+    <div class="Header-content">
+      <div class="Header-logo">
+        <h1>Catsgram</h1>
+      </div>
+      <div class="Header-nav">
+        <ul>
+          <li>❤</li>
+          {$likeCount === 0 ? '' : $likeCount} 
+          <li>🙍‍♂️</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/components/Share.svelte
@@ -0,0 +1,57 @@
+<style>
+  .Share {
+    border: 1px solid rgba(219, 219, 219, 1);
+    border-radius: 4px;
+    background-color: white;
+    margin: 0 0 2em 0;
+    width: 300px;
+    height: 80px;
+    position: absolute;
+    top: 300px;
+    left: 30%;
+    z-index: 9;
+    padding: 1em;
+  }
+  .Share-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .Share-head h2 {
+    font-size: 16px;
+  }
+  .Share-head button {
+    cursor: pointer;
+  }
+  .Share-content span {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: black;
+    font-size: 14px;
+    margin: 2em 0 0 0;
+  }
+  .Share-content a {
+    color: #3b5998;
+    margin: 0 0.5em 0 0;
+    font-size: 20px;
+  }
+</style>
+
+<div class="Share">
+  <div class="Share-head">
+    <h2>Compartir</h2>
+    <button on:click>
+      X
+    </button>
+  </div>
+  <div class="Share-content">
+    <a
+      href="https://www.facebook.com/sharer/sharer.php?&u=https://pugstagram.co"
+      target="_blank">
+      <span>
+      Compartir en Facebook 🌝
+      </span>
+    </a>
+  </div>
+</div>
